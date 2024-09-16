d607993b0f7f478a0531fd65b2fed313be807795
Ferdi ÜNAL <ferdiunal@outlook.com>
2024-09-16 21:04:33 +0300

    Refactor CSS to improve maintainability and readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import { generateTailwindColors } from '../../vendor/laravel/nova/generators'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./resources/**/*.{js,vue}'],
-  darkMode: ['class', '[class*="dark"]'],
+  darkMode: ['class', '[class="dark"]'],
   prefix: 'ns-',
   corePlugins: {
     preflight: false,
